e7dc2d6a1015c6c23256b69c29ad8c19aa392207
Luis Romero <luis.romero@atlascopco.com>
2025-11-20 15:14:12 +0100

    feat(client): Update weather API to fetch current and daily weather along with location name.

--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -1,15 +1,18 @@
 import { NextResponse } from 'next/server';
-import { getCurrentWeather } from '../../lib/weather/openweather.service';
+import { getCurrentAndDailyWeather, getLocationName } from '../../lib/weather/openweather.service';
 
 export const revalidate = 600;
 
 export async function GET() {
-  const current = await getCurrentWeather({ lat: 50.91, lon: 6.96, units: 'metric' });
+  const data = await getCurrentAndDailyWeather({ lat: 50.91, lon: 6.96, units: 'metric' });
+  const current = data.current;
+  const location = await getLocationName({ lat: 50.91, lon: 6.96 });
   return NextResponse.json({
     temp: current.temp,
     description: current.weather[0]?.description ?? '',
     id: current.weather[0]?.id ?? '',
     dateTime: current.weather[0]?.icon.includes('d') ? 'day' : 'night',
+    location,
     updatedAt: Date.now()
   });
 }
